3e45cef6cd459a5749ff8f7478709ebc91906c56
Abhishek Meena <abhishekmeena7452@gmail.com>
2023-10-08 16:29:39 -0700

    vecel changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import routes from "./routes/route.js";
 const app = express();
 
 app.use(cors());
-app.use(cors({credentials: true, origin: 'http://localhost:3000', methods: ['GET', 'POST', 'PUT', 'DELETE']}));
+app.use(cors({credentials: true, origin: 'https://gmail-frontend.vercel.app', methods: ['GET', 'POST']}));
 app.use(express.urlencoded());
 app.use(express.json());
 app.use("/", routes);
