0ca2aceb125de78df0bf1cdcb3a740f3d13f6433
Abhishek Meena <abhishekmeena7452@gmail.com>
2023-10-08 14:19:37 -0700

    db changed

--- databse/db.js
+++ databse/db.js
@@ -7,7 +7,8 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
-  const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@gmail-clone.imx0rpj.mongodb.net/?retryWrites=true&w=majority`;
+  // mongodb+srv://${USERNAME}:${PASSWORD}@gmail-clone.imx0rpj.mongodb.net/?retryWrites=true&w=majority
+  const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.i1zqx3s.mongodb.net/?retryWrites=true&w=majority`;
   try {
     mongoose.connect(DB_URI, { useNewUrlParser: true });
     mongoose.set("strictQuery", false);
