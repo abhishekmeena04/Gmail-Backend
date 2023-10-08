080c6e14611bba56a57504118fb7d0c563397777
Abhishek Meena <abhishekmeena7452@gmail.com>
2023-10-08 16:38:56 -0700

    engine added

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ import routes from "./routes/route.js";
 const app = express();
 
 app.use(cors());
-app.use(cors({credentials: true, origin: 'https://gmail-frontend.vercel.app', methods: ['GET', 'POST']}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://gmail-frontend.vercel.app",
+    methods: ["GET", "POST"],
+  })
+);
 app.use(express.urlencoded());
 app.use(express.json());
 app.use("/", routes);
